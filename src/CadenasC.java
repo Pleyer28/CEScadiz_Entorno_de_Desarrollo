a6eca73429a899937729e5a0bdf927ea3bf1eec9
AlbertoB <a.baurre@cesjuanpablosegundocadiz.es>
2022-03-15 21:56:09 +0100

    hola

---
+++ src/CadenasC.java
@@ -0,0 +1,17 @@
+
+public class CadenasC {
+
+	public static void main(String[] args) {
+		String cadena1= "personas";
+		String cadena2= "Importantes";
+		cadena1.concat(cadena2);
+		int cadena3=35;
+		String cadena4="Hay";
+		cadena4.concat(cadena1.concat(cadena2));
+		
+		System.out.println(cadena4.concat(cadena1.concat(cadena2))+cadena3);
+		
+
+	}
+
+}
